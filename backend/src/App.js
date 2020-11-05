ec9e126f02b7149eaff561c4c6dd4886b94fc960
Samuel-Victor <samuelvblima2@gmail.com>
2020-11-05 18:49:28 -0300

    fase1

---
+++ backend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../src/components/header/header';
+import Routes from './routes';
+
+function App() {
+  return (
+    <div className="App">
+   
+    </div>
+  );
+}
+
+export default App;
